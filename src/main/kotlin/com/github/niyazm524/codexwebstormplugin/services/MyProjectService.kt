4b2a912c2194edddee199b74beabc4ad36f522de
GitHub Action <action@github.com>
2026-01-23 20:07:34 +0000

    Template cleanup

--- src/main/kotlin/com/github/niyazm524/codexwebstormplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/niyazm524/codexwebstormplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.niyazm524.codexwebstormplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.niyazm524.codexwebstormplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
